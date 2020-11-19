4b7aacc94471964c14151928a43cf0173546b717
amalyn <amalynlaquinta@yahoo.com>
2020-11-19 20:06:51 +0800

    add gitignore and fix major issues

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+ -- Still in the `db` folder, create a `seeds.sql` file. In this file, \
+ -- write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('Double Quarter Pounder', true);
+
+
+
