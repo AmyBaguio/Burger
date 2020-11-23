ab67cb0870b94a3deffa8279e0656eaac31a68a3
amalyn <amalynlaquinta@yahoo.com>
2020-11-23 22:03:10 +0800

    91% complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,9 @@
  -- write insert queries to populate the `burgers` table with at least three entries.
 
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Filet');
 INSERT INTO burgers (name, devoured) VALUES ('Double Quarter Pounder', true);
+INSERT INTO burgers (name, devoured) VALUES ('Classic Angus', true);
 
 
 
